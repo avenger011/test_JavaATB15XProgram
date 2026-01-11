d303b3d36725ed309bdbceeecaeed03c83953fd9
ganesh <saiganeshrao01@gmail.com>
2026-01-11 17:10:47 +0530

    test

---
+++ src/main/java/org/example/question_12.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class question_12 {
+    public static void main(String[] args){
+
+        for(int i=0;i<11;i++)
+        {
+
+            System.out.println(i);
+            if(i==5)
+            {break;}
+        }
+
+
+    }
+}
