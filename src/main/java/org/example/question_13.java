d05452f7ae84d1eb8451de74ec9a0a14f901d2fd
ganesh <saiganeshrao01@gmail.com>
2026-01-11 17:11:17 +0530

    test

---
+++ src/main/java/org/example/question_13.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class question_13 {
+    public static void main(String[] args){
+
+        for(int i=0;i<11;i++)
+        { if(i==5)              // this will only skip 5, if we want to stop at 5
+                                        // use (i>5) wriyting this coz question is not clear
+            continue;
+            System.out.println(i);
+
+
+        }
+
+
+
+    }
+}
