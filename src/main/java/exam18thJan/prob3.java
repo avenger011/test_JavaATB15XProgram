e8ef2a548da6b72bcac496b1a42a7a724ca543bc
ganesh <saiganeshrao01@gmail.com>
2026-01-18 11:03:33 +0530

    test

---
+++ src/main/java/exam18thJan/prob3.java
@@ -0,0 +1,17 @@
+package exam18thJan;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class prob3 {
+    public static void main(String[] args){
+        System.out.println("enter the string to toconvrt to upper");
+        Scanner sc=new Scanner(System.in);
+        String str1=sc.next();
+
+        String str2=str1.toUpperCase();
+        System.out.println(str2);
+
+
+}
+    }
