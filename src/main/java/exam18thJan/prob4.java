1e2afca5e32a0b7812ed92fb43b28f703d2ae1f4
ganesh <saiganeshrao01@gmail.com>
2026-01-18 11:08:10 +0530

    test

---
+++ src/main/java/exam18thJan/prob4.java
@@ -0,0 +1,14 @@
+package exam18thJan;
+
+public class prob4 {
+    public static void main(String[] args){
+        String str1="hellow world";
+        String str2="Hellow WoRlD";
+
+        str1=str1.toUpperCase();
+        str2=str2.toUpperCase();
+        if(str1.equals(str2))
+        {System.out.println("the strings are same");}
+        else{System.out.println("the srings are not same");}
+    }
+}
